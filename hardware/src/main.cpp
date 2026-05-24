6b040c5f9153bd57d6ff039c4d9805ae3eee86cb
haerinnnnn <dungbd2610@gmail.com>
2026-05-24 18:03:54 +0700

    Add BLE iBeacon and update PlatformIO config
    Replace the placeholder Arduino sketch with a BLE iBeacon implementation that initializes BLE, sets TX power, constructs an iBeacon payload (manufacturer ID, UUID, major/minor, signal power) and starts advertising as "Museum Beacon". Update platformio.ini to target the esp32-s3-devkitc-1 board, set monitor speed, adjust flash/partition and PSRAM-related build options for a 16MB image, and add build flags (CORE_DEBUG_LEVEL, BOARD_HAS_PSRAM, ARDUINO_USB_*). Removes the previous example function and basic sketch in favor of the BLE beacon functionality.

--- hardware/src/main.cpp
+++ hardware/src/main.cpp
@@ -1,18 +1,55 @@
 #include <Arduino.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEBeacon.h>
+#include "esp_bt.h"
 
-// put function declarations here:
-int myFunction(int, int);
+#define BEACON_UUID "12345678-1234-1234-1234-123456789012"
+#define MAJOR 1
+#define MINOR 1
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+BLEAdvertising *pAdvertising;
+
+void setup()
+{
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(10);
+  }
+  
+  delay(1000);
+  Serial.println("Starting BLE");
+
+  BLEDevice::init("Museum Beacon");
+  esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_ADV, ESP_PWR_LVL_N12);
+
+  BLEBeacon oBeacon = BLEBeacon();
+  oBeacon.setManufacturerId(0x4C00);
+  oBeacon.setProximityUUID(BLEUUID(BEACON_UUID));
+  oBeacon.setMajor(MAJOR);
+  oBeacon.setMinor(MINOR);
+  oBeacon.setSignalPower(0xC5);
+
+  BLEAdvertisementData oAdvertisementData = BLEAdvertisementData();
+  oAdvertisementData.setFlags(0x04);
+  
+  std::string strServiceData = "";
+  strServiceData += (char)26;
+  strServiceData += (char)0xFF;
+  strServiceData += oBeacon.getData();
+  oAdvertisementData.addData(strServiceData);
+
+  pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->setAdvertisementData(oAdvertisementData);
+  pAdvertising->setScanResponse(false);
+
+  pAdvertising->start();
+  Serial.println("Advertising");
+  
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  delay(2000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
